0886ec522f9d5aec80478d3ded1152867b636c72
Carlos Eduardo Obristo <162042804+caduobristo@users.noreply.github.com>
2024-04-18 15:46:57 -0300

    Add files via upload
    tb update

--- ULA_tb.vhd
+++ ULA_tb.vhd
@@ -1,37 +1,66 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use IEEE.math_real.all;
 
 entity ULA_tb is
-end;
+end entity ULA_tb;
 
-architecture a_ULA_tb of ULA_tb is
-    component ULA
-        port( entrada0, entrada1 : in unsigned(15 downto 0);
-              controle : in unsigned(1 downto 0);
-              saida : out unsigned(15 downto 0)
-        );
-    end component;
-    signal entrada0, entrada1, saida : unsigned(15 downto 0);
-    signal controle : unsigned(1 downto 0);
+architecture testbench of ULA_tb is
+
+    signal entrada0_tb, entrada1_tb : unsigned(15 downto 0);
+    signal controle_tb : unsigned(1 downto 0);
+    signal saida_tb : unsigned(15 downto 0);
+
 begin
-    uut: ULA port map( entrada0 => entrada0,
-                       entrada1 => entrada1,
-                       saida => saida,
-                       controle => controle);
-    entrada0 <= "0000000000000100";
-    entrada1 <= "0000000000000010";
-    process
-    begin
-        controle <= "00";
-        wait for 50 ns;
-        controle <= "01";
-        wait for 50 ns;
-        controle <= "10";
-        wait for 50 ns;
-        controle <= "11";
-        wait for 50 ns;
-        wait;
-    end process;
 
-end architecture a_ULA_tb;
+    ULA_inst : entity work.ULA
+        port map (
+            entrada0 => entrada0_tb,
+            entrada1 => entrada1_tb,
+            controle => controle_tb,
+            saida => saida_tb
+        );
+
+    processo_teste: process
+    begin
+        -- casos base
+        entrada0_tb <= to_unsigned(3, 16);
+        entrada1_tb <= to_unsigned(1, 16);
+        controle_tb <= "00";
+        wait for 2 ns;
+        
+        entrada0_tb <= to_unsigned(4, 16);
+        entrada1_tb <= to_unsigned(2, 16);
+        controle_tb <= "01";
+        wait for 2 ns;
+        
+        entrada0_tb <= to_unsigned(2, 16);
+        entrada1_tb <= to_unsigned(2, 16);
+        controle_tb <= "10";
+        wait for 2 ns;
+        
+        entrada0_tb <= to_unsigned(2, 16);
+        entrada1_tb <= to_unsigned(3, 16);
+        controle_tb <= "11";
+        wait for 2 ns;
+        
+        -- casos de interesse
+        --entrada0_tb <= to_unsigned(2, 16);
+        --entrada1_tb <= to_unsigned(4, 16);
+        --controle_tb <= "01";
+        --wait for 2 ns;
+
+        entrada0_tb <= "1111111111111111";
+        entrada1_tb <= "0111111111111111";
+        controle_tb <= "11";
+        wait for 2 ns;
+
+        entrada0_tb <= "1111111111111111";
+        entrada1_tb <= "0111111111111111";
+        controle_tb <= "10";
+        wait for 2 ns;
+        
+    end process processo_teste;
+
+end architecture testbench;

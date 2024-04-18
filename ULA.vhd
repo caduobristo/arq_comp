157e0d5158a632a533dda7c1750dd200288a552b
Carlos Eduardo Obristo <162042804+caduobristo@users.noreply.github.com>
2024-04-18 17:00:19 -0300

    Add files via upload
    flags attempt, error: bound check failure

--- ULA.vhd
+++ ULA.vhd
@@ -6,8 +6,9 @@ use IEEE.math_real.all;
 entity ULA is
     port(
         entrada0, entrada1 : in unsigned(15 downto 0);
-        controle : in unsigned(1 downto 0);
-        saida : out unsigned(15 downto 0)
+        controle           : in unsigned(1 downto 0);
+        saida              : out unsigned(15 downto 0);
+        negativo, carry    : out std_logic := '0'
     );
 end entity ULA;
 
@@ -17,11 +18,26 @@ begin
     begin
         case controle is
             when "00" =>
-                saida <= entrada0+entrada1;
+                if entrada0+entrada1 > "1111111111111111" then
+                    carry <= '1';
+                    saida <= resize(entrada0+entrada1, 16);
+                else
+                    saida <= entrada0+entrada1;
+                end if;
             when "01" =>
-                saida <= entrada0-entrada1;
+                if entrada1 > entrada0 then
+                    negativo <= '1';
+                    saida <= entrada1-entrada0;
+                else
+                    saida <= entrada0-entrada1;
+                end if;
             when "10" =>
-                saida <= resize(entrada0*entrada1, 16);
+                if entrada0*entrada1 > "1111111111111111" then
+                    carry <= '1';
+                    saida <= resize(entrada0*entrada1, 16);
+                else
+                    saida <= entrada0*entrada1;
+                end if;
             when "11" => 
                 saida <= to_unsigned(integer(real(to_integer(entrada0))**real(to_integer(entrada1))), 16);
             when others => 
--- ULA_tb.vhd
+++ ULA_tb.vhd
@@ -1,26 +1,34 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-use IEEE.math_real.all;
 
 entity ULA_tb is
 end entity ULA_tb;
 
-architecture testbench of ULA_tb is
+architecture a_ULA_tb of ULA_tb is
+    component ULA
+        port( 
+            entrada0, entrada1 : in unsigned(15 downto 0);
+            controle           : in unsigned(1 downto 0);
+            saida              : out unsigned(15 downto 0);
+            negativo, carry    : out std_logic := '0'
+        );
+    end component;
 
     signal entrada0_tb, entrada1_tb : unsigned(15 downto 0);
-    signal controle_tb : unsigned(1 downto 0);
-    signal saida_tb : unsigned(15 downto 0);
+    signal controle_tb              : unsigned(1 downto 0);
+    signal saida_tb                 : unsigned(15 downto 0);
+    signal negativo_tb, carry_tb    : std_logic;
 
 begin
 
-    ULA_inst : entity work.ULA
-        port map (
-            entrada0 => entrada0_tb,
-            entrada1 => entrada1_tb,
-            controle => controle_tb,
-            saida => saida_tb
-        );
+    uut : ULA port map( entrada0 => entrada0_tb,
+                        entrada1 => entrada1_tb,
+                        controle => controle_tb,
+                        negativo => negativo_tb,
+                        carry    => carry_tb,
+                        saida    => saida_tb
+                    );
 
     processo_teste: process
     begin
@@ -45,22 +53,23 @@ begin
         controle_tb <= "11";
         wait for 2 ns;
         
-        -- casos de interesse
-        --entrada0_tb <= to_unsigned(2, 16);
-        --entrada1_tb <= to_unsigned(4, 16);
-        --controle_tb <= "01";
-        --wait for 2 ns;
+        --casos de interesse
+        entrada0_tb <= to_unsigned(2, 16);
+        entrada1_tb <= to_unsigned(4, 16);
+        controle_tb <= "01";
+        wait for 2 ns;
 
         entrada0_tb <= "1111111111111111";
-        entrada1_tb <= "0111111111111111";
+        entrada1_tb <= "1111111111111111";
         controle_tb <= "11";
         wait for 2 ns;
 
         entrada0_tb <= "1111111111111111";
-        entrada1_tb <= "0111111111111111";
+        entrada1_tb <= "1111111111111111";
         controle_tb <= "10";
         wait for 2 ns;
+        wait;
         
     end process processo_teste;
 
-end architecture testbench;
+end architecture a_ULA_tb;

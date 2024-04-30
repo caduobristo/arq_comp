90ba277589b8a33732bb9b731521d5227c55cfc9
Carlos Eduardo Obristo <162042804+caduobristo@users.noreply.github.com>
2024-04-30 11:05:57 -0300

    Delete script.bat

--- script.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Users\caduo\OneDrive\Documentos\Facul\arq_comp\vhdl\lab02\ULA
-ghdl -a ULA.vhd
-ghdl -e ULA
-ghdl -a ULA_tb.vhd
-ghdl -e ULA_tb
-ghdl -r ULA_tb --wave=ULA_tb.ghw
-gtkwave ULA_tb.ghw

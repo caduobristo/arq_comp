3e0c90c7e69ef2198a2062c458155d94694eecc8
Carlos Eduardo Obristo <162042804+caduobristo@users.noreply.github.com>
2024-05-01 13:18:34 -0300

    bound error

--- ban_reg.vhd
+++ ban_reg.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity ban_reg is
     port ( read1, read2, wrt  : in unsigned(2 downto 0);
            data_in            : in unsigned(15 downto 0);
-           wr_en, rst, clk    : in std_logic;
+           clk, rst, wr_en    : in std_logic;
            reg_out1, reg_out2 : out unsigned(15 downto 0)
     );
 end ban_reg;
@@ -18,33 +18,17 @@ architecture a_ban_reg of ban_reg is
            );
     end component;  
 
-    signal reg1, reg2, reg3, reg4, reg5, reg6, reg7, data_out : unsigned(15 downto 0); 
-    signal reg0 : unsigned(15 downto 0) := "0000000000000000";
+    signal reg1, reg2, reg3, reg4, reg5, reg6, reg7 : unsigned(15 downto 0);
+    signal zero : unsigned(15 downto 0) := "0000000000000000";
 
-begin
-    uut: reg16bits port map( clk => clk, rst => rst, wr_en => wr_en,
-                             data_in => data_in, data_out => data_out);
-    
-    process(clk) is
-    begin
-        case wrt is
-            when "001" =>
-                reg1 <= data_out;
-            when "010" =>
-                reg2 <= data_out;
-            when "011" =>
-                reg3 <= data_out;
-            when "100" =>
-                reg4 <= data_out;
-            when "101" =>
-                reg5 <= data_out;
-            when "110" =>
-                reg6 <= data_out;
-            when "111" =>
-                reg7 <= data_out;
-            when others =>
-        end case;
-    end process;
+begin   
+    register1: reg16bits port map(clk => clk, rst => rst, wr_en => wr_en, data_in => data_in, data_out => reg1);
+    register2: reg16bits port map(clk => clk, rst => rst, wr_en => wr_en, data_in => data_in, data_out => reg1);
+    register3: reg16bits port map(clk => clk, rst => rst, wr_en => wr_en, data_in => data_in, data_out => reg1);
+    register4: reg16bits port map(clk => clk, rst => rst, wr_en => wr_en, data_in => data_in, data_out => reg1);
+    register5: reg16bits port map(clk => clk, rst => rst, wr_en => wr_en, data_in => data_in, data_out => reg1);
+    register6: reg16bits port map(clk => clk, rst => rst, wr_en => wr_en, data_in => data_in, data_out => reg1);
+    register7: reg16bits port map(clk => clk, rst => rst, wr_en => wr_en, data_in => data_in, data_out => reg1);
 
     reg_out1 <= reg1 when read1 = "001" else
                 reg2 when read1 = "010" else
@@ -53,7 +37,7 @@ begin
                 reg5 when read1 = "101" else
                 reg6 when read1 = "110" else
                 reg7 when read1 = "111" else
-                reg0;
+                zero;
         
     reg_out2 <= reg1 when read2 = "001" else
                 reg2 when read2 = "010" else
@@ -62,6 +46,6 @@ begin
                 reg5 when read2 = "101" else
                 reg6 when read2 = "110" else
                 reg7 when read2 = "111" else
-                reg0; 
+                zero; 
         
 end a_ban_reg;
